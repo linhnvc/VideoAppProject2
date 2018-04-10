872bcab2f6468758168fe7d9241879f0335586d9
nguyen van cong linh <conglinh@Cong-Linh.local>
2018-04-10 21:48:09 +0700

    Handle Play and Pause Video

--- CongLinhApp/Helpers/VideoLauncher.swift
+++ CongLinhApp/Helpers/VideoLauncher.swift
@@ -9,9 +9,124 @@
 import UIKit
 import AVFoundation
 
+//class VideoPlayerView: UIView {
+//
+//    //Bieu tuong loading giua man hinh
+//    let activityIndicatorView: UIActivityIndicatorView = {
+//        let act = UIActivityIndicatorView(activityIndicatorStyle: .whiteLarge)
+//        act.translatesAutoresizingMaskIntoConstraints = false
+//        act.startAnimating()
+//        return act
+//    }()
+//
+//    //Nut tam dung video:
+//    lazy var pausePlayButton: UIButton = {
+//        let button = UIButton()
+//        let image = UIImage(named: "trending")
+//        button.setImage(image, for: .normal)
+//        button.tintColor = .white
+//        //button.isHidden = true
+//        button.translatesAutoresizingMaskIntoConstraints = false
+//        button.addTarget(self, action: #selector(handlePause), for: .touchUpInside)
+//        return button
+//    }()
+//
+//    var isPlaying = false
+//
+//    //Tam dung video:
+//    @objc func handlePause() {
+//        if isPlaying {
+//            player?.pause()
+//            print("Paused")
+//            pausePlayButton.setImage(UIImage(named: "subscriptions"), for: .normal) //play
+//        } else {
+//            player?.play()
+//            print("Playing")
+//            pausePlayButton.setImage(UIImage(named: "trending"), for: .normal) //pause
+//        }
+//        isPlaying = !isPlaying
+//    }
+//
+//    // controlsContainerView se la man che o phia tren cua video dang chay
+//    let controlsContainerView: UIView = {
+//        let view = UIView()
+//        view.backgroundColor = UIColor(white: 0, alpha: 0.1)
+//        return view
+//    }()
+//
+//    override init(frame: CGRect) {
+//        super.init(frame: frame)
+//
+//        setupPlayerView()
+//
+//        //View nay se o phia tren video dang chay
+//        controlsContainerView.frame = frame
+//        addSubview(controlsContainerView)
+//
+//        //Bieu tuong loading giua man hinh
+//        controlsContainerView.addSubview(activityIndicatorView)
+//        activityIndicatorView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+//        activityIndicatorView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+//
+//        controlsContainerView.addSubview(pausePlayButton)
+//        pausePlayButton.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
+//        pausePlayButton.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+//        pausePlayButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
+//        pausePlayButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+//
+//        backgroundColor = .blue
+//
+//    }
+//
+//    var player: AVPlayer?
+//
+//
+//    func setupPlayerView() {
+//        let urlString = "https://firebasestorage.googleapis.com/v0/b/gameofchats-762ca.appspot.com/o/message_movies%2F12323439-9729-4941-BA07-2BAE970967C7.mov?alt=media&token=3e37a093-3bc8-410f-84d3-38332af9c726"
+//        if let url = NSURL(string: urlString) {
+//            player = AVPlayer(url: url as URL)
+//
+//            let playerLayer = AVPlayerLayer(player: player)
+//            self.layer.addSublayer(playerLayer)
+//            playerLayer.frame = self.frame
+//
+//            player?.play()
+//
+//
+//            player?.addObserver(self, forKeyPath: key, options: .new, context: nil)
+//        }
+//    }
+//
+//    let key: String = "key"
+//
+//    //Khi load xong video:
+//    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+//        if keyPath == key {
+//            print("Done")
+//            activityIndicatorView.stopAnimating()
+//            controlsContainerView.backgroundColor = UIColor.clear
+//            pausePlayButton.isHidden = false
+//            isPlaying = true
+//        }
+//    }
+//
+//
+//
+//    required init?(coder aDecoder: NSCoder) {
+//        fatalError("init(coder:) has not been implemented")
+//    }
+//}
+
+
+
+
+
+
+
+
+
 class VideoPlayerView: UIView {
     
-    //Bieu tuong loading giua man hinh
     let activityIndicatorView: UIActivityIndicatorView = {
         let act = UIActivityIndicatorView(activityIndicatorStyle: .whiteLarge)
         act.translatesAutoresizingMaskIntoConstraints = false
@@ -19,72 +134,237 @@ class VideoPlayerView: UIView {
         return act
     }()
     
-    //Nut tam dung video:
-    let pauseButton: UIButton = {
-        let button = UIButton()
-        let image = UIImage(named: "pause")
+    lazy var pausePlayButton: UIButton = {
+        let button = UIButton(type: .system)
+        let image = UIImage(named: "trending")
         button.setImage(image, for: .normal)
-        button.tintColor = .white
         button.translatesAutoresizingMaskIntoConstraints = false
+        button.tintColor = .white
+        button.isHidden = true
+        
+        button.addTarget(self, action: #selector(handlePause), for: .touchUpInside)
+        
         return button
     }()
     
-    // controlsContainerView se la man che o phia tren cua video dang chay
+    var isPlaying = false
+    
+    @objc func handlePause() {
+        if isPlaying {
+            player?.pause()
+            pausePlayButton.setImage(UIImage(named: "subscriptions"), for: .normal)
+        } else {
+            player?.play()
+            pausePlayButton.setImage(UIImage(named: "trending"), for: .normal)
+        }
+        
+        isPlaying = !isPlaying
+    }
+    
+    let videoLengthLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.text = "00:00"
+        lbl.textColor = .white
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        lbl.font = UIFont.boldSystemFont(ofSize: 13)
+        lbl.textAlignment = .right
+        return lbl
+    }()
+    
+    let currentTimeLabel: UILabel = {
+        let lbl = UILabel()
+        lbl.text = "00:00"
+        lbl.textColor = .white
+        lbl.translatesAutoresizingMaskIntoConstraints = false
+        lbl.font = UIFont.boldSystemFont(ofSize: 13)
+        return lbl
+    }()
+    
+    lazy var videoSlider: UISlider = {
+        let slider = UISlider()
+        slider.translatesAutoresizingMaskIntoConstraints = false
+        slider.minimumTrackTintColor = UIColor.red
+        slider.maximumTrackTintColor = .white
+        //slider.setThumbImage(UIImage(named: ""), for: .normal)
+        slider.addTarget(self, action: #selector(handleSliderChange), for: .valueChanged)
+        return slider
+    }()
+    
+    @objc func handleSliderChange() {
+        print(videoSlider.value)
+        //videoSlider.value = so phan cua slider duoc keo di
+        
+        if let duration = player?.currentItem?.duration {
+            let totalSeconds = CMTimeGetSeconds(duration) //tong do dai video theo don vi giay
+            let value = Float64(videoSlider.value) * totalSeconds //thoi diem can chuyen den
+            let seekTime = CMTime(value: Int64(value), timescale: 1)
+            player?.seek(to: seekTime, completionHandler: { (completedSeek) in
+                
+            })
+        }
+        
+    }
+    
     let controlsContainerView: UIView = {
         let view = UIView()
-        view.backgroundColor = UIColor(white: 0, alpha: 0.3)
+        view.backgroundColor = UIColor(white: 0, alpha: 1)
         return view
     }()
+    
+    let titleVideoLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Cong Linh"
+        label.textColor = UIColor.white
+        label.font = UIFont.boldSystemFont(ofSize: 14)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let titleVideo: UILabel = {
+        let label = UILabel()
+        label.text = "Hương Tràm và Công Linh công khai tình \ncảm"
+        label.numberOfLines = 2
+        label.textColor = UIColor.black
+        label.font = UIFont.boldSystemFont(ofSize: 20)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+    let numberOfViews: UILabel = {
+        let label = UILabel()
+        label.text = "1.098.084 lượt xem"
+        label.numberOfLines = 2
+        label.textColor = UIColor.darkGray
+        label.font = UIFont.systemFont(ofSize: 14)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    
+//    let chooseMenu: MenuBar = {
+//        let cm = MenuBar()
+//        return cm
+//    }()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
         
         setupPlayerView()
+        setupGradientLayer()
         
-        //View nay se o phia tren video dang chay
         controlsContainerView.frame = frame
         addSubview(controlsContainerView)
+        addSubview(titleVideo)
+        addSubview(numberOfViews)
+        //addSubview(chooseMenu)
         
-        //Bieu tuong loading giua man hinh
-        //controlsContainerView.addSubview(activityIndicatorView)
-        //activityIndicatorView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
-        //activityIndicatorView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        //chooseMenu.topAnchor.constraint(equalTo: numberOfViews.topAnchor, constant: 5).isActive = true
+        //chooseMenu.leftAnchor.constraint(equalTo: leftAnchor, constant: 10).isActive = true
         
-        controlsContainerView.addSubview(pauseButton)
-        pauseButton.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
-        pauseButton.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
-        pauseButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
-        pauseButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        titleVideo.topAnchor.constraint(equalTo: controlsContainerView.bottomAnchor, constant: 10).isActive = true
+        titleVideo.leftAnchor.constraint(equalTo: leftAnchor, constant: 10).isActive = true
+        //titleVideo.heightAnchor.constraint(equalToConstant: 50).isActive = true
         
-        backgroundColor = .blue
+        numberOfViews.topAnchor.constraint(equalTo: titleVideo.bottomAnchor, constant: 5).isActive = true
+        numberOfViews.leftAnchor.constraint(equalTo: leftAnchor, constant: 10).isActive = true
         
+        controlsContainerView.addSubview(titleVideoLabel)
+        titleVideoLabel.topAnchor.constraint(equalTo: topAnchor, constant: 5).isActive = true
+        titleVideoLabel.leftAnchor.constraint(equalTo: leftAnchor, constant: 10).isActive = true
+        
+        
+        controlsContainerView.addSubview(activityIndicatorView)
+        activityIndicatorView.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        activityIndicatorView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        
+        controlsContainerView.addSubview(pausePlayButton)
+        pausePlayButton.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
+        pausePlayButton.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
+        pausePlayButton.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        pausePlayButton.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        
+        controlsContainerView.addSubview(videoLengthLabel)
+        videoLengthLabel.rightAnchor.constraint(equalTo: rightAnchor, constant: -9).isActive = true
+        videoLengthLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -2).isActive = true
+        videoLengthLabel.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        videoLengthLabel.heightAnchor.constraint(equalToConstant: 25).isActive = true
+        
+        controlsContainerView.addSubview(currentTimeLabel)
+        currentTimeLabel.leftAnchor.constraint(equalTo: leftAnchor, constant: 9).isActive = true
+        currentTimeLabel.bottomAnchor.constraint(equalTo: bottomAnchor, constant: -2).isActive = true
+        currentTimeLabel.widthAnchor.constraint(equalToConstant: 50).isActive = true
+        currentTimeLabel.heightAnchor.constraint(equalToConstant: 25).isActive = true
+        
+        controlsContainerView.addSubview(videoSlider)
+        videoSlider.rightAnchor.constraint(equalTo: videoLengthLabel.leftAnchor).isActive = true
+        videoSlider.leftAnchor.constraint(equalTo: currentTimeLabel.rightAnchor).isActive = true
+        videoSlider.bottomAnchor.constraint(equalTo: bottomAnchor).isActive = true
+        videoSlider.heightAnchor.constraint(equalToConstant: 30).isActive = true
+        
+        backgroundColor = .black
     }
     
-    func setupPlayerView() {
+    var player: AVPlayer?
+    
+    private func setupPlayerView() {
+
         let urlString = "https://firebasestorage.googleapis.com/v0/b/gameofchats-762ca.appspot.com/o/message_movies%2F12323439-9729-4941-BA07-2BAE970967C7.mov?alt=media&token=3e37a093-3bc8-410f-84d3-38332af9c726"
         if let url = NSURL(string: urlString) {
-            let player = AVPlayer(url: url as URL)
+            player = AVPlayer(url: url as URL)
             
             let playerLayer = AVPlayerLayer(player: player)
             self.layer.addSublayer(playerLayer)
             playerLayer.frame = self.frame
             
-            player.play()
+            player?.play()
             
+            player?.addObserver(self, forKeyPath: "currentItem.loadedTimeRanges", options: .new, context: nil)
             
-            //player.addObserver(self, forKeyPath: "done", options: .new, context: nil)
+            let interval = CMTime(value: 1, timescale: 2)
+            player?.addPeriodicTimeObserver(forInterval: interval, queue: DispatchQueue.main, using: { (progressTime) in
+                
+                //Chay thoi gian khi video playing
+                let seconds = CMTimeGetSeconds(progressTime)
+                let secondsString = String(format: "%02d", Int(seconds) % 60)
+                let minutesString = String(format: "%02d", Int(seconds) / 60)
+                self.currentTimeLabel.text = "\(minutesString):\(secondsString)"
+                
+                //Chay slider:
+                if let duration = self.player?.currentItem?.duration {
+                    let durationSeconds = CMTimeGetSeconds(duration)
+                    self.videoSlider.value = Float(seconds / durationSeconds)
+                }
+            })
         }
     }
     
-    //An xoay tron loading giua man hinh khi da load xong video:
-//    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
-//        if keyPath == "done" {
-//            print("Done")
-//            activityIndicatorView.stopAnimating()
-//            controlsContainerView.backgroundColor = UIColor.clear
-//        }
-//    }
+    //Khi load xong va bat dau play video
+    override func observeValue(forKeyPath keyPath: String?, of object: Any?, change: [NSKeyValueChangeKey : Any]?, context: UnsafeMutableRawPointer?) {
+        if keyPath == "currentItem.loadedTimeRanges" {
+            //print("Done")
+            activityIndicatorView.stopAnimating()
+            controlsContainerView.backgroundColor = .clear
+            pausePlayButton.isHidden = false
+            isPlaying = true
+            
+            //Lay va Hien thi do dai video
+            if let duration = player?.currentItem?.duration {
+                let seconds = CMTimeGetSeconds(duration)
+                let secondsText = Int(seconds) % 60
+                //Hien thi so phut voi 2 chu so:
+                let minutesText = String(format: "%02d", Int(seconds) / 60)
+                videoLengthLabel.text = "\(minutesText):\(secondsText)"
+
+            }
+        }
+    }
     
+    private func setupGradientLayer() {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = bounds
+        gradientLayer.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
+        gradientLayer.locations = [0.7, 1.2]
+        controlsContainerView.layer.addSublayer(gradientLayer)
+    }
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
